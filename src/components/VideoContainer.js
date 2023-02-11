75bfcc8815dfd4f7f2c9bd6fe5e85c252a81fb77
Kartik Gupta <kartikgupta966@gmail.com>
2023-02-11 21:46:14 +0530

    feat: videos added

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { YOUTUBE_API } from "../utils/constants";
+import VideoCard from "./VideoCard";
 
 const VideoContainer = () => {
-  return (
-    <div>VideoContainer</div>
-  )
-}
+  const [videos, setVideos] = useState([]);
 
-export default VideoContainer
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  const getVideos = async () => {
+    const res = await fetch(YOUTUBE_API);
+    const data = await res.json();
+    console.log(data);
+    setVideos(data.items);
+  };
+
+  return (
+    <div className="flex flex-wrap">
+      {videos.map(video => (
+        <VideoCard key={video.id} info={video} />
+      ))}
+    </div>
+  );
+};
+
+export default VideoContainer;
---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+const GOOGLE_API_KEY = "AIzaSyCjtXYMqkjEYzmJdo2oFUKZ9vutOqrYiXU";
+
+export const YOUTUBE_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=${GOOGLE_API_KEY}`
