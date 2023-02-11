1078c6933bb4b71e6a0d72d75f6f0cac2117a1e6
Kartik Gupta <kartikgupta966@gmail.com>
2023-02-11 20:08:48 +0530

    feat: header comp with logo

---
+++ src/components/Head.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "../App.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaUserAlt } from "react-icons/fa";
+import { HiOutlineSearch } from "react-icons/hi";
+
+const Head = () => {
+  return (
+    <div className="grid grid-flow-col shadow-lg">
+      <div className="col-span-1 flex items-center px-4 gap-4">
+        <GiHamburgerMenu className="cursor-pointer" />
+        <img
+          src="https://logos-world.net/wp-content/uploads/2020/04/YouTube-Logo.png"
+          className="w-28"
+        />
+      </div>
+
+      <div className="col-span-9 flex items-center justify-center">
+        <input
+          type="text"
+          placeholder="Search"
+          className="border-2 border-gray-400 w-1/2 p-1.5 rounded-l-full indent-4"
+        />
+        <HiOutlineSearch className="w-auto h-10 p-2.5 bg-gray-200 rounded-r-full" />
+      </div>
+
+      <div className="grid-rows-2 flex items-center">
+        <FaUserAlt />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
